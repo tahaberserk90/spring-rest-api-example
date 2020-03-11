dc6e2b854306bd333caa0a5676e7996f3c793989
tahaberserk90 <tahaberserk@gmail.com>
2020-03-11 17:08:07 +0100

    start example

---
+++ src/test/java/com/rest/api/spring/SpringRestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.api.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
